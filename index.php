5773129281372a1ef4baf6b70fa50965125a0c8a
just52155 <just52155@gmail.com>
2017-12-12 00:21:54 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<p >
+    <strong><span > &nbsp;</span>
+</p>
+<p>
+    &nbsp;
+</p>
+<p>
+    <span >日在北京召开，建研爱康携绿色建筑领域的新技术、新产品全程参与。</span>
+</p>
+<p>
+    &nbsp;
+</p>
+<p >
+    <img src="http://image.jy-ak.com/data/upload/ueditor/20160827/57c069240cfdf.jpg" alt="image001.jpg" height="375" width="500"/>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span >&nbsp; &nbsp; 建研爱康参展的新技术主题为：新风、测霾、建筑气密性的研究成果，展会上通过产品展示、资料宣传、现场讲解等新时态的方式全面分享并推广。</span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span >&nbsp; &nbsp;建研爱康的工作人员为参观者讲解并演示新产品的技术性能，通过专业性的展示彰显建研爱康的技术能力和技术水准。</span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span >&nbsp; &nbsp; &nbsp;展会期间，住建部建筑节能与科技司司长杨榕、北京市住宅建筑设计研究院有限公司胡颐蘅副院长、中国建筑科学研究院院长王俊、中国建筑科学研究院环能院院 长徐伟、副院长路宾莅临指导，对建研爱康的节能技术服务能力给与高度认可，并共同展望了今后建筑节能领域发展的前景。</span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <img src="http://image.jy-ak.com/data/upload/ueditor/20160827/57c069297f9b2.jpg" alt="image016.jpg" />
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <span /></span>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <img src="http://image.jy-ak.com/data/upload/ueditor/20160827/57c0692b95e7e.jpg" alt="image023.jpg" height="281" width="500"/>
+</p>
+<p >
+    &nbsp;
+</p>
+<p >
+    <img src="http://image.jy-ak.com/data/upload/ueditor/20160827/57c0692c3dac1.jpg" alt="image026.jpg" height="375" width="500"/>
+</p>
+<p>
+    <br/>
+</p>
